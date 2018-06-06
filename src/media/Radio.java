975cf0ff847655d5f1b71ad30d71d966211ef45c
Marecare <qa-prepodne@gmail.com>
2018-06-06 11:53:51 +0200

    Created class Radio under package media
    Created get and set methods for attributes: fmFrequency, amFrequency, band

---
+++ src/media/Radio.java
@@ -0,0 +1,40 @@
+
+package media;
+
+
+public class Radio {
+
+    private double fmFrequency;
+    private int amFrequency;
+    private char band;
+    
+    public double getFmFrequency() {
+        return this.fmFrequency; 
+    }
+    
+    public void setFmFrequency(double newFmFrequency) {
+        this.fmFrequency = newFmFrequency;
+    }
+    
+    public int getAmFrequency() {
+        return this.amFrequency; 
+    }
+    
+    public void setAmFrequency(int newAmFrequency) {
+        this.amFrequency = newAmFrequency;
+    }
+    
+    public char getBand() {
+        return this.band; 
+    }
+    
+    public void setBand(char newBand) {
+        this.band = newBand;
+    }
+    
+    
+    
+    
+    
+    
+}
